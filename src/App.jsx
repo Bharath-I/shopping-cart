11a87ecf4674653f27a5512ef09a2478dcbb7d5d
Bharath <bharath19h142@gmail.com>
2024-06-29 11:53:12 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import './App.css';
+import { Head } from './Head';
+import { Nav } from './Nav';
+import { Footer } from './Footer';
+import { Section } from './Section';
+import { useState } from 'react';
+
+
+function App() {
+
+  var data = [
+    {
+      name: "Fancy Product",
+      price: "$40.00 - $80.00",
+      rating: "",
+      cart: "Add to Cart",
+      poster: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+    },
+    {
+      name: "Special Item",
+      price: ["$20.00", "$18.00"],
+      rating: "⭐⭐⭐⭐⭐",
+      cart: "Add to Cart",
+      poster: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+    },
+    {
+      name: "Sale Item",
+      price: ["$50.00", "$25.00"],
+      rating: "",
+      cart: "Add to Cart",
+      poster: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+    },
+    {
+      name: "Popular Item",
+      price: "$40.00",
+      rating: "⭐⭐⭐⭐⭐",
+      cart: "Add to Cart",
+      poster: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+    },
+    {
+      name: "Sale Item",
+      price: ["$50.00", "$25.00"],
+      rating: "",
+      cart: "Add to Cart",
+      poster: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+    },
+    {
+      name: "Fancy Product",
+      price: "$120.00 - $280.00",
+      rating: "",
+      cart: "Add to Cart",
+      poster: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+    },
+    {
+      name: "Special Item",
+      price: ["$20.00", "$18.00"],
+      rating: "⭐⭐⭐⭐⭐",
+      cart: "Add to Cart",
+      poster: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+    },
+    {
+      name: "Popular Item",
+      price: "$40.00",
+      rating: "⭐⭐⭐⭐⭐",
+      cart: "Add to Cart",
+      poster: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+    }
+  ]
+  const [count, setCount] = useState(0)
+  return (
+    <div >
+        <Nav count={count} setCount={setCount}/>
+        <Head />
+        <Section count={count} setCount={setCount} details={data}/>
+        <Footer />
+    </div>
+  );
+}
+
+
+export default App;
